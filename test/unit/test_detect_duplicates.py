9a010943e350faa439d60feb55a4c92bdf8d16a3
albinr <albin@rybergs.net>
2025-08-28 12:03:49 +0200

    Added start to tests

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -1,7 +1,11 @@
 import pytest
-
+from src.util.detector import detect_duplicates
+# from src.util.parser import parser, Article
 # develop your test cases here
 
 @pytest.mark.unit
-def test_detect_duplicates():
-    assert True
+# TC04 Empty list input
+def test_empty_list():
+    with pytest.raises(ValueError):
+        detect_duplicates("")
+    
